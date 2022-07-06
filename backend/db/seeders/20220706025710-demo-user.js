e379c21a2577cdb1124f2fcbe637b2b7f828ea8d
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-07-05 20:36:23 -0700

    set up

---
+++ backend/db/seeders/20220706025710-demo-user.js
@@ -0,0 +1,31 @@
+'use strict';
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password'),
+      },
+      {
+        email: "thefakeemail1@gmail.com",
+        username: 'FakeUser1',
+        hashedPassword: bcrypt.hashSync('password1'),
+      },
+      {
+        email: "theFAKEEMAIL2@gmail.com",
+        username: 'FakeUser2',
+        hashedPassword: bcrypt.hashSync('password2'),
+      },
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2'] }
+    }, {});
+  }
+};
