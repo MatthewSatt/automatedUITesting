3171450d5243f910d33aee3d2ac98e813623d07e
Matthew <MatthewSatterwhiteMS@gmail.com>
2022-07-09 21:45:20 -0700

    homepage;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Route, Switch } from 'react-router-dom';
 import SignupFormPage from './components/SignupFormPage';
 import LoginFormPage from "./components/LoginFormPage";
 import * as sessionActions from './store/session';
 import Navigation from './components/Navigation';
+import Home from './components/Home';
 import { Modal } from './context/Modal';
 // import LoginForm from './components/LoginFormModal/LoginForm';
 function App() {
   const dispatch = useDispatch();
+  const user = useSelector(state => state.session.user);
   const [isLoaded, setIsLoaded] = useState(false);
   const [showModal, setShowModal] = useState(false);
   useEffect(() => {
@@ -22,9 +25,11 @@ function App() {
         <Switch>
           <Route path="/login" >
             <LoginFormPage />
+            <Home />
           </Route>
           <Route path='/signup'>
             <SignupFormPage />
+            <Home />
           </Route>
         </Switch>
       )}
